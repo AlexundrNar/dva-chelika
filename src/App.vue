506589edc77ebf828bfba5a9fbf9c1ee29a99363
Otrebje <Guiltygearrev1@gmail.com>
2024-08-14 01:55:00 +0600

    step 1

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed ,ref } from 'vue' 
+import ItemsSelected from './components/ItemsSelected.vue'
+import ItemsInventory from './components/ItemsInventory.vue'
+
+const leftData = ref([])
+const rightData = ref({})
+
+const addToLeft = (item) => {
+  leftData.value.length < 6 ? leftData.value.push(item) : null
+}
+
+const deleteFromLeft = (id) => {
+  leftData.value = leftData.value.filter(item => item.id !== id)
+}
+
+const addToRight = (item) => {
+  rightData.value = item
+}
+
+
+</script>
+
+<template>
+  <main class="main">
+    <ItemsSelected :leftArray="leftData" :rightObj="rightData" @deleteFromLeft="deleteFromLeft"/>
+    <ItemsInventory @fromLeft="addToLeft" @fromRight="addToRight"/>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  width: 100vw;
+  max-width: 1440px;
+  padding: 0 20px;
+  margin: 0 auto;
+  padding-top: 60px;
+}
+</style>
---
+++ src/components/ItemsSelected.vue
@@ -0,0 +1,80 @@
+<script setup>
+defineProps({
+  leftArray: Array,
+  rightObj: {}
+})
+</script>
+
+<template>
+  <section class="section">
+    <div class="block left">
+      <div v-for="item in leftArray" @click="$emit('deleteFromLeft', item.id)">{{ item.name }}</div>
+      <p>selected: {{ leftArray.length }} / 6</p>
+    </div>
+
+    <div class="block right">
+      <div>
+        <p>{{ rightObj.id }}</p>
+        <p>{{ rightObj.name }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.section {
+  display: flex;
+  justify-content: space-between;
+}
+
+.block {
+  position: relative;
+  width: 300px;
+  height: 170px;
+  
+  background-color: black;
+  border-radius: 15px;
+
+  div {
+    line-height: 48px;
+    text-align: center;
+    background-color: greenyellow;
+    border-radius: 15px;
+    cursor: pointer;
+  }
+}
+
+.left {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 20px 20px 50px;
+  gap: 12px;
+
+  div {
+    height: max-content;
+    &:hover {
+      scale: 1.08;
+    }
+  }
+
+  p {
+    position: absolute;
+    bottom: 10px;
+    left: 0;
+    right: 0;
+    color: white;
+    text-align: center;
+  }
+}
+
+.right {
+  padding: 20px;
+
+  div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+</style>
